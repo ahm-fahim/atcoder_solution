edb6ae51dd9b0a96a27d0fdc0e0fbab2558de8de
ahm-fahim <contactahmfahim@gmail.com>
2023-10-28 19:05:35 +0600

    solved

--- pb7.cpp
+++ pb7.cpp
@@ -1,5 +1,33 @@
 // https://atcoder.jp/contests/abc326/tasks/abc326_a
+
 #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    if (x > y) {
+        int res_up = x - y;
+        if (res_up <= 3) {
+            cout << "Yes" << '\n';
+        } else {
+            cout << "No" << '\n';
+        }
+    }
+    if (x < y) {
+        int res_down = y - x;
+        if (res_down <= 2) {
+            cout << "Yes" << '\n';
+        } else {
+            cout << "No" << '\n';
+        }
+    }
+
+    return 0;
+}
+
+/*#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -14,4 +42,4 @@ int main()
     }
 
     return 0;
-}
+}*/
